84bf5510a709b00a9a5a79dd85b095831bff641c
mohanjnv1 <mohanjnv1@gmail.com>
2020-01-17 22:10:13 +0530

    Initial commit

---
+++ app/src/main/java/com/example/alcheringaadmin2020/MainActivity.java
@@ -0,0 +1,16 @@
+package com.example.alcheringaadmin2020;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    private String serverKey = "AAAAyClnqTE:APA91bHNYLgZZdL91IOdFWB23oV7YJ9SPhiRu7DsNBLqUuf1UwUdXIHgA7argtzWB_DHi5vWB4q7nBvpIMIc9S0oZDIdGXFPLegaSh-oEhdBSeqhyMZJwNTTrQ4TyBXwEyX0qPL5RQUo";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
